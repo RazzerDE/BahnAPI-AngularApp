c469544cad2612db8647ead0003146ca0b617469
Doepner, Yanic <yanic.doepner@sulzer.de>
2024-11-19 09:58:43 +0100

    tableComponent: add table to trainStation

--- src/app/pages/train-station/train-station.component.ts
+++ src/app/pages/train-station/train-station.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import {TableComponent} from "../../util/table/table.component";
 
 @Component({
   selector: 'app-train-station',
   standalone: true,
-  imports: [],
+  imports: [
+    TableComponent
+  ],
   templateUrl: './train-station.component.html',
   styleUrl: './train-station.component.css'
 })
 export class TrainStationComponent {
   public currentTrainStation: string = "Mageburg Hbf";
+  public tableHeaders: string[] = ['Adresse/Koordinaten', 'Betreiber', 'Internet vorhanden', 'barrierefrei', 'hat lokale Transportmöglichkeiten']
+  public tableData: string[][] = [
+    ['123 Main St , 74.0060° W', 'Company A', 'Yes', 'Yes', 'Yes']
+  ];
 
   changeTrainStation(): void {
     const searchInput = document.getElementById('searchStation') as HTMLInputElement;
