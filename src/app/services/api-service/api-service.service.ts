963005b283e47c52c04feb5e066913f7b212dedd
Yannic Drews <Yannic.Drews@sulzer.de>
2024-11-19 11:05:14 +0100

    implemented new api-service

--- src/app/services/api-service/api-service.service.ts
+++ src/app/services/api-service/api-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {environment} from "./types/environment";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
+  private API_URL: string | undefined = environment.API_URL;
+  private API_KEY: string | undefined = environment.API_KEY;
 
-  constructor() { }
+
+
+  constructor(private http: HttpClient) {}
+
+
 }
