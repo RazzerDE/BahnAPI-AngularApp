6f6778893c3ba2852e5e90ffbe6e823334e5eb9b
Yannic Drews <Yannic.Drews@sulzer.de>
2024-11-20 10:25:04 +0100

    Stop tracking environment.ts

--- src/app/services/api-service/types/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  API_URL: "https://apis.deutschebahn.com/db-api-marketplace/apis/",
-  API_KEY: "api-secret-schluessel-hier-einfuegen",
-  API_ID: "api-id-hier-einfuegen"
-}
