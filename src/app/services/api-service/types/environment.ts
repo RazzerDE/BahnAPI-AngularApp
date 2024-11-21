afdae135412fa4f30516f20f2e87371a61b96ad9
Yannic Drews <Yannic.Drews@sulzer.de>
2024-11-21 10:46:40 +0100

    updated environment data

--- src/app/services/api-service/types/environment.ts
+++ src/app/services/api-service/types/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   API_URL: "https://apis.deutschebahn.com/db-api-marketplace/apis/",
-  API_KEY: "03a3e2ba3f8e9d4ee32d5e980692cfb0",
-  API_ID: "82ddf5919b6feda93973cf1c52c0c1f3"
+  API_KEY: "dein-api-schluessel-hier",
+  API_ID: "deine-api-client-id-hier"
 }
 
